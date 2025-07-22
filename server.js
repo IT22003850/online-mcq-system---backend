c8042789cff1e6aa8b89244514c9b89b6ecd259a
Thilina Thushara <thilina.sam.a7@gmail.com>
2025-07-22 17:47:20 +0530

    cors updated

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://<your-netlify-app>.netlify.app"],
+    origin: ["http://localhost:5173", "https://online-mcq-system.netlify.app"],
     credentials: true,
   })
 );
