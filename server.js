5652cc54b2437663556d451daf49a3bb0b340544
Thilina Thushara <thilina.sam.a7@gmail.com>
2025-07-22 10:45:33 +0530

    add colors to console logs

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express')
+const colors = require('colors')
 
 const app = express()
 
 app.listen(5000,() => {
-    console.log(`Server is listning to port: 5000`)
+    console.log(`Server is listning to port: 5000`.blue)
 })
