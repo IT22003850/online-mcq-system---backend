c137cc77d615728819d92cd867774c42e9281e89
Thilina Thushara <thilina.sam.a7@gmail.com>
2025-07-22 11:19:10 +0530

    user model implimented

---
+++ models/userModel.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+  },
+  { timetamps: true }
+);
+
+module.exports = mongoose.model('User', userSchema);
