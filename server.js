779087fd24647d306fe7865e82cd3f5727da816b
Thilina Thushara <thilina.sam.a7@gmail.com>
2025-07-22 16:56:17 +0530

    bug fixes

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const connectDB = require("./config/dbConfig");
 const dotenv = require("dotenv").config();
 const userRoutes = require("./routes/userRoutes");
 const examRoutes = require("./routes/examRoutes");
-const resultsRoutes = require('./routes/resultsRoute')
+const resultsRoutes = require("./routes/resultsRoute");
+const cors = require("cors");
 
 const PORT = process.env.PORT;
 
@@ -15,12 +16,17 @@ const app = express();
 // Other middleware
 app.use(express.json());
 
-app.use(require("cors")());
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://<your-netlify-app>.netlify.app"],
+    credentials: true,
+  })
+);
 
 //Routes
 app.use("/api/users", userRoutes);
 app.use("/api/exams", examRoutes);
-app.use("/api/results", resultsRoutes)
+app.use("/api/results", resultsRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is listning to port: ${PORT}`.blue);
